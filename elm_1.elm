8501db82e9ef243dadff2cde3e6fce6993a64465
vasina <dashvas@rambler.ru>
2016-06-28 19:00:13 +0300

    add comments

--- elm_1.elm
+++ elm_1.elm
@@ -1,3 +1,5 @@
+--Project 1: 2 buttons with + and -, changing the number N in between 3 and 12.
+                                                    
 module Main exposing (..)
 
 import Html exposing (Html, div, button, text)
@@ -5,24 +7,28 @@ import Html.Events exposing (onClick)
 import Html.App
 import Mouse
 
--- MODEL
 
+-- MODEL - state of counter
 type alias Model =
       Int
 
+
+-- Init function: counter initial state is 3; 
+-- do nothing, so Cmd.none
 init : ( Model, Cmd Msg )
 init =
      ( 3, Cmd.none )
 
--- MESSAGES
 
+-- MESSAGES - set of messages that we will get from the UI
+-- Messages will trigger when one of the buttons or mouse are pressed.
 type Msg
     = Decrement
     |Increment
     |MouseMsg Mouse.Position
 
--- VIEW
 
+-- VIEW - a way to view state as HTML 
 view : Model -> Html Msg
 view model =
       div []
@@ -31,8 +37,8 @@ view model =
           , button [ onClick Increment ] [ text "+" ]
           ]
 
--- UPDATE
 
+-- UPDATE describes what to do when receive one of messages.
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
         case msg of
@@ -45,7 +51,7 @@ update msg model =
            MouseMsg position ->
                 ( model, Cmd.none )
 
--- SUBSCRIPTIONS
+-- SUBSCRIPTIONS declares the things we want to listen to
 subscriptions : Model -> Sub Msg
 subscriptions model =
      Mouse.clicks MouseMsg
