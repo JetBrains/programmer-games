f13f25e7a0912d2c5c8efafcb40af0aabfb64708
vasina <dashvas@rambler.ru>
2016-07-11 13:48:00 +0300

    add print all MachineCfg

--- Turing/src/Main.elm
+++ Turing/src/Main.elm
@@ -1,31 +1,30 @@
 import Html exposing (Html, div, button, text)
 import Turing exposing(..)
 
-
--- Symbol is a ball of wool with special color
--- Tape head is a kitten
+-- Symbol is a ball of wool with special color                                  
+-- Tape head is a kitten  
 type BallOfWool = Red | Orange | Yellow | Green | Blue 
 type Kitten = Grey | Black | Brown | LightGrey
 
-
--- | A test Turing machine 
+-- | A test Turing machine
 testMachine =
-        { transition = t
-        , startState = LightGrey -- = 0 
-        , acceptState = Brown  -- = 2
-        , rejectState = Black  -- = 3
-        }
+  { transition = t
+  , startState = LightGrey 
+  , acceptState = Brown 
+  , rejectState = Black
+  }
  
+-- | A transition function
 t : Kitten -> Maybe BallOfWool -> (Kitten, BallOfWool, Turing.Direction)
 t st sm =
-        case st of
-                LightGrey -> case sm of
-                        Just x -> (LightGrey, x, MoveRight)
-                        Nothing -> (Grey, Red, Turing.MoveLeft)
-                Grey -> case sm of
-                        Just x -> (Grey, x, Turing.MoveLeft)
-                        Nothing -> (Brown, Blue, Turing.MoveRight)
-                _ -> (Black, Yellow, Turing.MoveLeft)
+  case st of
+    LightGrey -> case sm of
+      Just x -> (LightGrey, x, MoveRight)
+      Nothing -> (Grey, Red, Turing.MoveLeft)
+    Grey -> case sm of
+      Just x -> (Grey, x, Turing.MoveLeft)
+      Nothing -> (Brown, Blue, Turing.MoveRight)
+    _ -> (Black, Yellow, Turing.MoveLeft)
 
 main =
-        text (runMachine testMachine [Red, Orange, Yellow, Green, Blue])
+  text (runMachine testMachine [Red, Orange, Yellow, Green, Blue])
--- Turing/src/Turing.elm
+++ Turing/src/Turing.elm
@@ -1,3 +1,4 @@
+-- BlueRedOrangeYellowGreenBlueRed qBrown 
 module Turing exposing (..)
 
 import Html exposing (Html, div, button, text)
@@ -5,22 +6,22 @@ import Array exposing (..)
 import String exposing (..)
 import List exposing (..)
 
--- BlueRedOrangeYellowGreenBlueRed qBrown  
 
 -- | Tape head movement direction.
 type Direction = MoveLeft | MoveRight
 
+
 type alias Machine a b = 
   {
-  -- | @'transition' s smb@ defines the behaviour of the machine when it
-  -- is in state @s@ and @smb@ is the symbol under the tape head.
-  -- The function returns the next state of the machine,
-  -- the symbol overwriting the current symbol under the tape head,
-  -- and the direction in which the tape head should move after overwriting.
-
-  -- If @smb@ is @'Just' x@, that signifies that @x@ is the current symbol
-  -- under the tape head.
-  -- If @smb@ is 'Nothing', that means that the tape head is over a
+  -- | @'transition' s smb@ defines the behaviour of the machine when it        
+  -- is in state @s@ and @smb@ is the symbol under the tape head.               
+  -- The function returns the next state of the machine,                        
+  -- the symbol overwriting the current symbol under the tape head,             
+  -- and the direction in which the tape head should move after overwriting.    
+  
+  -- If @smb@ is @'Just' x@, that signifies that @x@ is the current symbol      
+  -- under the tape head.                                                       
+  -- If @smb@ is 'Nothing', that means that the tape head is over a             
   -- tape position that has not been initialised yet.
 
   transition    : b -> Maybe a -> (b, a, Direction)
@@ -30,15 +31,15 @@ type alias Machine a b =
   }
 
 
-type alias TapeCfg a = 
-  { leftSyms : Array a  -- ^ symbols to the left of tape head
-  , currSym : Maybe a  -- ^ symbol under the tape head
-  , rightSyms : Array a  -- ^ symbols to the right of tape head
-  }       
+type alias TapeCfg a =                                                          
+  { leftSyms : Array a    --  symbols to the left of tape head                   
+  , currSym : Maybe a     --  symbol under the tape head                          
+  , rightSyms : Array a   --  symbols to the right of tape head                 
+  } 
 
 
 type alias MachineCfg a b =
-  { currState : b     -- current state of machine
+  { currState : b         -- current state of machine
   , tapeCfg   : TapeCfg a
   }
 
@@ -49,7 +50,7 @@ updateTapeCfg {leftSyms, currSym, rightSyms} newSym dir =
   case dir of     
     MoveLeft ->
       let 
-      -- Add element to the left end of a sequence to the right of tape head
+        -- Add element to the left end of a sequence to the right of tape head 
         right = (Array.append (Array.repeat 1 newSym) rightSyms) 
       in
         if (Array.isEmpty leftSyms) then (TapeCfg empty Nothing right) 
@@ -57,7 +58,7 @@ updateTapeCfg {leftSyms, currSym, rightSyms} newSym dir =
              (Array.get ((Array.length leftSyms)-1) leftSyms) right)
     MoveRight ->
       let
-      -- Add element to the right end of a sequence to the left of tape head
+        -- Add element to the right end of a sequence to the left of tape head
         left = (Array.push newSym leftSyms)
       in
         if (Array.isEmpty rightSyms) then (TapeCfg left Nothing empty)
@@ -65,12 +66,12 @@ updateTapeCfg {leftSyms, currSym, rightSyms} newSym dir =
              (Array.slice 1 (Array.length rightSyms) rightSyms))
 
 
--- | Execute one transition step for given machine and config.
+-- | Execute one transition step for given machine and config. 
 updateMachineCfg : Machine a b -> MachineCfg a b -> MachineCfg a b
 updateMachineCfg m {currState, tapeCfg} =
   let (state', newSym, dir) = m.transition currState tapeCfg.currSym
   in (updateTapeCfg tapeCfg newSym dir)
-     |> MachineCfg state' -- f <| x == f x  x |> f == f x
+     |> MachineCfg state' 
 
 
 -- | Initialise tape with input word.
@@ -83,7 +84,6 @@ initTapeCfg w =
 
 -- | Initialise machine config with input word.
 initMachineCfg : Machine a b -> List a -> MachineCfg a b
--- init tape with input word and define current state with start state
 initMachineCfg m input = MachineCfg (m.startState) (initTapeCfg input)
 
 
@@ -93,15 +93,17 @@ machineCfgFinal m mcfg =
   mcfg.currState == m.acceptState || mcfg.currState == m.rejectState
 
 
--- run machine while not in final state. 
--- return the last machine configuration (state+tape)
-run : Machine a b -> MachineCfg a b -> MachineCfg a b
-run m mcfg = 
-  if (machineCfgFinal m mcfg) then mcfg
-  else (run m (updateMachineCfg m mcfg))  
+-- run machine while not in final state.                                        
+run : Machine a b -> MachineCfg a b -> List (MachineCfg a b) -> List (MachineCfg a b)
+run m mcfg res = 
+  let 
+      upd = (updateMachineCfg m mcfg)
+  in 
+     if (machineCfgFinal m mcfg) then res
+     else ( run m upd (res ++ [upd]) )  
 
 
--- print tape --> return tape as a list of symbols
+-- print tape --> return tape as a list of strings
 printTapeCfg : TapeCfg a -> List String
 printTapeCfg {leftSyms, currSym, rightSyms} = 
   case currSym of
@@ -111,14 +113,18 @@ printTapeCfg {leftSyms, currSym, rightSyms} =
                (Array.toList (Array.map toString rightSyms)) ++ [" "]
 
 
--- print machine ---> return it as a string with the tape and the last state
+-- print machine --> return it as a string with the tape and the last state
 printMachineCfg : MachineCfg a b -> String 
 printMachineCfg {currState, tapeCfg} =
-  String.concat [String.concat (printTapeCfg tapeCfg), "q", 
-                                            (toString currState)]
+-- (repeat (Array.length tapeCfg.leftSyms) " ") ++ "| q" ++ (toString currState)
+  String.concat [String.concat (printTapeCfg tapeCfg), "q", (toString currState)]
 
 
--- | Return the last machine config for given input word until final state.
+-- | Return all machine configs for given input word until final state.
 runMachine : Machine a b -> List a -> String
 runMachine m w = 
-        printMachineCfg (run m (initMachineCfg m w)) 
+  let 
+    init = (initMachineCfg m w)
+  in 
+--    String.join (String.fromChar '\n') (List.map printMachineCfg (run m init [init]))
+    String.join " /// " (List.map printMachineCfg (run m init [init]))
