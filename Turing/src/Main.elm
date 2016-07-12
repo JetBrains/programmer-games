6d46328abf029d3326daa3c1fc74138bf8f35901
vasina <dashvas@rambler.ru>
2016-07-12 14:15:21 +0300

    Turing.elm works with List (Maybe a) as an input. TransF return (..,Maybe a, ..) now. need to do print more beautiful

--- Turing/src/Main.elm
+++ Turing/src/Main.elm
@@ -1,30 +1,41 @@
-import Html exposing (Html, div, button, text)
+import Html exposing (text)
 import Turing exposing(..)
 
-type BallOfWool = Red | Orange | Yellow | Green | Blue | DarkGrey
-type Kitten = Grey | Black | Brown | LightGrey
 
+type BallOfWool = Red | Orange | Yellow | Green | Blue -- a
+type Kitten = Grey | Black | Brown | LightGrey -- b
+
+
+testMachine : Machine BallOfWool Kitten
 testMachine =
-  { transition = (transFunc transTable (Black, DarkGrey, MoveLeft))
+  { transition = (transFunc transTable (Black, Nothing, MoveLeft))
   , startState = LightGrey 
   , acceptState = Brown 
   , rejectState = Black
   }
 
+
+transTable : TransTable BallOfWool Kitten
 transTable = 
-  [ { key = (LightGrey, Just Red), value = (LightGrey, Red, MoveRight)}
-  , { key = (LightGrey, Just Orange), value = (LightGrey, Orange, MoveRight)}
-  , { key = (LightGrey, Just Yellow), value = (LightGrey, Yellow, MoveRight)}
-  , { key = (LightGrey, Just Green), value = (LightGrey, Green, MoveRight)}
-  , { key = (LightGrey, Just Blue), value = (LightGrey, Blue, MoveRight)}
-  , { key = (LightGrey, Nothing), value = (Grey, Red, MoveLeft)}
-  , { key = (Grey, Just Red), value = (Grey, Red, MoveLeft)}
-  , { key = (Grey, Just Orange), value = (Grey, Orange, MoveLeft)}
-  , { key = (Grey, Just Yellow), value = (Grey, Yellow, MoveLeft)}
-  , { key = (Grey, Just Green), value = (Grey, Green, MoveLeft)}
-  , { key = (Grey, Just Blue), value = (Grey, Blue, MoveLeft)}
-  , { key = (Grey, Nothing), value = (Brown, Blue, MoveRight)}
+  [ { key = (LightGrey, Just Red), value = (LightGrey, Just Red, MoveRight)}
+  , { key = (LightGrey, Just Orange), value = (LightGrey, Just Orange, MoveRight)}
+  , { key = (LightGrey, Just Yellow), value = (LightGrey, Just Yellow, MoveRight)}
+  , { key = (LightGrey, Just Green), value = (LightGrey, Just Green, MoveRight)}
+  , { key = (LightGrey, Just Blue), value = (LightGrey, Just Blue, MoveRight)}
+  , { key = (LightGrey, Nothing), value = (Grey, Just Red, MoveLeft)}
+  , { key = (Grey, Just Red), value = (Grey, Just Red, MoveLeft)}
+  , { key = (Grey, Just Orange), value = (Grey, Just Orange, MoveLeft)}
+  , { key = (Grey, Just Yellow), value = (Grey, Just Yellow, MoveLeft)}
+  , { key = (Grey, Just Green), value = (Grey, Just Green, MoveLeft)}
+  , { key = (Grey, Just Blue), value = (Grey, Just Blue, MoveLeft)}
+  , { key = (Grey, Nothing), value = (Brown, Just Blue, MoveRight)}
   ] 
 
+
+input : List (Maybe BallOfWool)
+input = 
+  [Just Red, Nothing, Just Orange, Just Yellow, Just Green, Just Blue]
+
+
 main =
-  text (runMachine testMachine [Red, Orange, Yellow, Green, Blue])
+  text (runMachine testMachine input)
--- Turing/src/Turing.elm
+++ Turing/src/Turing.elm
@@ -1,12 +1,12 @@
--- BlueRedOrangeYellowGreenBlueRed qBrown 
 module Turing exposing (..)
 
-import Html exposing (Html, div, button, text)
-import Array exposing (..)
-import String exposing (..)
-import List exposing (..)
-import Dict exposing (..)
+import Array as Arr exposing (..)
+import String as Str exposing (..)
+import List exposing (..) 
 
+
+--TYPES BLOCK------------------------------------------------------------------
+-------------------------------------------------------------------------------
 
 -- | Tape head movement direction.
 type Direction = MoveLeft | MoveRight
@@ -14,18 +14,7 @@ type Direction = MoveLeft | MoveRight
 
 type alias Machine a b = 
   {
-  -- | @'transition' s smb@ defines the behaviour of the machine when it        
-  -- is in state @s@ and @smb@ is the symbol under the tape head.               
-  -- The function returns the next state of the machine,                        
-  -- the symbol overwriting the current symbol under the tape head,             
-  -- and the direction in which the tape head should move after overwriting.    
-  
-  -- If @smb@ is @'Just' x@, that signifies that @x@ is the current symbol      
-  -- under the tape head.                                                       
-  -- If @smb@ is 'Nothing', that means that the tape head is over a             
-  -- tape position that has not been initialised yet.
-
-  transition    : (b, Maybe a) -> (b, a, Direction)
+  transition    : (b, Maybe a) -> (b, Maybe a, Direction)
   , startState  : b
   , acceptState : b 
   , rejectState : b
@@ -33,9 +22,9 @@ type alias Machine a b =
 
 
 type alias TapeCfg a =                                                          
-  { leftSyms  : Array a   --  symbols to the left of tape head                   
-  , currSym   : Maybe a   --  symbol under the tape head                          
-  , rightSyms : Array a   --  symbols to the right of tape head                 
+  { leftSyms  : Array (Maybe a)   --  symbols to the left of tape head                   
+  , currSym   : Maybe a           --  symbol under the tape head                          
+  , rightSyms : Array (Maybe a)   --  symbols to the right of tape head                 
   } 
 
 
@@ -47,55 +36,141 @@ type alias MachineCfg a b =
 
 type alias KeyValue a b =
   { key : (b, Maybe a)
-  , value : (b, a, Direction)
+  , value : (b, Maybe a, Direction)
   }
+
 
 type alias TransTable a b = 
   List (KeyValue a b)
+------------------------------------------------------------------------------
 
+
+--HELPERS TO UPDATE BLOCK-----------------------------------------------------
+------------------------------------------------------------------------------
+
+empty : Array (Maybe a)
+empty = Arr.fromList [Nothing] 
+
+
+getNewRight : Array (Maybe a) -> Maybe a -> Array (Maybe a)
+getNewRight right sym =
+  (Arr.append (Arr.repeat 1 sym) right)
+
+
+getNewLeft : Array (Maybe a) -> Maybe a -> Array (Maybe a) 
+getNewLeft left sym = 
+  (Arr.push sym left)  
+
+
+getLast : Array (Maybe a) -> Maybe a
+getLast leftS =
+  case (Arr.get ((Arr.length leftS)-1) leftS) of
+    Just x -> x
+    Nothing -> Nothing
+
+
+withoutLast : Array (Maybe a) -> Array (Maybe a)
+withoutLast leftS = 
+  (Arr.slice 0 -1 leftS)
+
+
+moveLeft : Array (Maybe a) -> Array (Maybe a) -> TapeCfg a 
+moveLeft leftSyms right =
+  if leftSyms == empty then (TapeCfg empty Nothing right)    
+  else (TapeCfg (withoutLast leftSyms) (getLast leftSyms) right) 
+
+
+getTail : Array (Maybe a) -> Array (Maybe a)
+getTail rightS =
+  (Arr.slice 1 (Arr.length rightS) rightS)
+
+
+getFirst : Array (Maybe a) -> Maybe a
+getFirst rightS =
+  case (Arr.get 0 rightS) of
+    Just x -> x
+    Nothing -> Nothing
+
+
+moveRight : Array (Maybe a) -> Array (Maybe a) -> TapeCfg a 
+moveRight rightSyms left =
+  if rightSyms == empty then (TapeCfg left Nothing empty)    
+  else (TapeCfg left (getFirst rightSyms) (getTail rightSyms))    
+
+
+doTrans : Machine a b -> MachineCfg a b -> (b, Maybe a, Direction)              
+doTrans m {currState, tapeCfg} =                                                
+  m.transition (currState, tapeCfg.currSym)  
+------------------------------------------------------------------------------
+
+
+--UPDATE BLOCK----------------------------------------------------------------
+------------------------------------------------------------------------------
 
 -- | Replace symbol under tape head with new symbol, then move tape head.
-updateTapeCfg : TapeCfg a -> a -> Direction -> TapeCfg a
-updateTapeCfg {leftSyms, currSym, rightSyms} newSym dir =
+updateTapeCfg : TapeCfg a -> Maybe a -> Direction -> TapeCfg a
+updateTapeCfg tcfg newSym dir =
   case dir of     
-    MoveLeft ->
-      let 
-        -- Add element to the left end of a sequence to the right of tape head 
-        right = (Array.append (Array.repeat 1 newSym) rightSyms) 
-      in
-        if (Array.isEmpty leftSyms) then (TapeCfg Array.empty Nothing right) 
-        else (TapeCfg (Array.slice 0 -1 leftSyms) 
-             (Array.get ((Array.length leftSyms)-1) leftSyms) right)
-    MoveRight ->
-      let
-        -- Add element to the right end of a sequence to the left of tape head
-        left = (Array.push newSym leftSyms)
-      in
-        if (Array.isEmpty rightSyms) then (TapeCfg left Nothing Array.empty)
-        else (TapeCfg left (Array.get 0 rightSyms) 
-             (Array.slice 1 (Array.length rightSyms) rightSyms))
+    MoveLeft -> 
+      ( moveLeft tcfg.leftSyms (getNewRight tcfg.rightSyms newSym) )
+    MoveRight -> 
+      ( moveRight tcfg.rightSyms (getNewLeft tcfg.leftSyms newSym) )
 
 
 -- | Execute one transition step for given machine and config. 
 updateMachineCfg : Machine a b -> MachineCfg a b -> MachineCfg a b
-updateMachineCfg m {currState, tapeCfg} =
-  let (state', newSym, dir) = m.transition (currState, tapeCfg.currSym)
-  in (updateTapeCfg tapeCfg newSym dir)
-     |> MachineCfg state' 
+updateMachineCfg m mcfg =
+  let 
+    (newState, newSym, dir) = (doTrans m mcfg)
+  in 
+    (updateTapeCfg mcfg.tapeCfg newSym dir)
+    |> MachineCfg newState 
 
+
+------------------------------------------------------------------------------
+
+
+--INIT BLOCK------------------------------------------------------------------
+------------------------------------------------------------------------------
 
 -- | Initialise tape with input word.
-initTapeCfg : List a -> TapeCfg a
+initTapeCfg : List (Maybe a) -> TapeCfg a
 initTapeCfg w = 
   case w of 
-    [] -> TapeCfg Array.empty Nothing Array.empty
-    (x::xs) -> TapeCfg Array.empty (Just x) (Array.fromList xs)  
+    -- if not Array.empty but Array.fromList [Nothing]
+    -- [] -> TapeCfg Array.empty Nothing Array.empty 
+    -- (x::xs) -> TapeCfg Array.empty x (Array.fromList xs)  
+    [] -> TapeCfg empty Nothing empty
+    (x::xs) -> TapeCfg empty x (Arr.fromList xs)
 
 
 -- | Initialise machine config with input word.
-initMachineCfg : Machine a b -> List a -> MachineCfg a b
+initMachineCfg : Machine a b -> List (Maybe a) -> MachineCfg a b
 initMachineCfg m input = MachineCfg (m.startState) (initTapeCfg input)
+------------------------------------------------------------------------------
 
+
+--PRINT BLOCK-----------------------------------------------------------------
+------------------------------------------------------------------------------
+
+-- print tape --> return tape as a list of strings                              
+printTapeCfg : TapeCfg a -> List String                                         
+printTapeCfg {leftSyms, currSym, rightSyms} =                                   
+  (Arr.toList (Arr.map toString leftSyms)) ++ [toString currSym] ++       
+  (Arr.toList (Arr.map toString rightSyms))                  
+
+
+-- print machine --> return it as a string with the tape and the last state     
+printMachineCfg : MachineCfg a b -> String                                      
+printMachineCfg {currState, tapeCfg} =                                          
+  Str.concat [ Str.join " " (printTapeCfg tapeCfg), 
+               " current_machine_state: ", (toString currState),           
+               " current_symbol_on_the_tape: ", (toString tapeCfg.currSym)]              
+------------------------------------------------------------------------------
+
+
+--RUN BLOCK-------------------------------------------------------------------
+------------------------------------------------------------------------------
 
 -- | Return true if the machine is in a final state.
 machineCfgFinal : Machine a b -> MachineCfg a b -> Bool
@@ -113,32 +188,8 @@ run m mcfg res =
      else ( run m upd (res ++ [upd]) )  
 
 
--- print tape --> return tape as a list of strings
-printTapeCfg : TapeCfg a -> List String
-printTapeCfg {leftSyms, currSym, rightSyms} = 
-  case currSym of
-    Just c -> (Array.toList (Array.map toString leftSyms)) ++ [toString c] ++
-                     (Array.toList (Array.map toString rightSyms))
-    Nothing -> (Array.toList (Array.map toString leftSyms)) ++ 
-               (Array.toList (Array.map toString rightSyms))
-
-
--- print machine --> return it as a string with the tape and the last state
-printMachineCfg : MachineCfg a b -> String 
-printMachineCfg {currState, tapeCfg} =
-  case tapeCfg.currSym of
-    Just x -> 
-      String.concat [String.concat (printTapeCfg tapeCfg), 
-                    " current_machine_state: ", (toString currState),
-                    " current_symbol_on_the_tape: ", (toString x)]
-    Nothing -> 
-      String.concat [String.concat (printTapeCfg tapeCfg),                      
-                    " current_machine_state: q", (toString currState),          
-                    " current_symbol_on_the_tape: Nothing"]
-
-
 -- | A transition function
-transFunc : TransTable a b -> (b, a, Direction) -> (b, Maybe a) -> (b, a, Direction)
+transFunc : TransTable a b -> (b, Maybe a, Direction) -> (b, Maybe a) -> (b, Maybe a, Direction)
 transFunc tt def key = 
   case (head tt) of
     Just h -> if h.key == key 
@@ -151,9 +202,10 @@ transFunc tt def key =
 
 
 -- | Return all machine configs for given input word until final state.
-runMachine : Machine a b -> List a -> String
+runMachine : Machine a b -> List (Maybe a) -> String
 runMachine m w = 
   let 
     init = (initMachineCfg m w)
   in 
-    String.join " /// " (List.map printMachineCfg (run m init [init]))
+    Str.join " /// " (List.map printMachineCfg (run m init [init]))
+------------------------------------------------------------------------------
