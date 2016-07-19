cad43a730a2f367ad93f037d265de5339e1036ec
vasina <dashvas@rambler.ru>
2016-07-19 17:02:02 +0300

    draw cat table baskets balls mirror in Main.elm, add img dir (first commit)

--- Turing/src/Main.elm
+++ Turing/src/Main.elm
@@ -1,39 +1,53 @@
-import Html exposing (text)
-import TuringTypes exposing (Direction(..), Machine, TransTable)
+import Html exposing (Html, div, text)
+import Html.Attributes exposing (..)                                            
+import Html.App  
+
+import TuringTypes exposing ( Direction(..), Machine, MachineCfg, TapeCfg, 
+                              TransTable )
 import RunTuring exposing (debugRun, transFunc)                          
+import InitUpdate exposing (initMachineCfg)
+
+import Svg exposing (Svg)                                                       
+import Svg.Attributes exposing (..)                                             
+
+import Window                                                                   
+import Task                                                                     
+import Mouse  
+
+import List exposing (head, drop)
+import Array exposing (empty)
 
 
-type BallOfWool = Red | Orange | Yellow | Green | Blue -- a
-type Kitten = Grey | Black | Brown | LightGrey -- b
+------------------------------------------------------------------------------
+type BallOfWool = Red | Yellow | Green | Blue -- a
+--white is start, orange is natural, violet is reject
+type Kitten = White | Grey | DarkGrey | Orange | Violet -- b 
 
 
-testMachine : Machine BallOfWool Kitten
-testMachine =
-  { transition = (transFunc transTable (Black, Nothing, MoveLeft))
-  , startState = LightGrey 
-  , acceptState = Brown 
-  , rejectState = Black
+machine : Machine BallOfWool Kitten
+machine =
+  { transition = (transFunc transTable (Violet, Nothing, MoveLeft))
+  , initHeadPos = 1   
+  , startState = White
+  , acceptState = Orange 
+  , rejectState = Violet
   }
 
 
 transTable : TransTable BallOfWool Kitten
 transTable = 
-  [ { key = (LightGrey, Just Red), 
-      value = (LightGrey, Just Red, MoveRight)}
-  , { key = (LightGrey, Just Orange), 
-      value = (LightGrey, Just Orange, MoveRight)}
-  , { key = (LightGrey, Just Yellow), 
-      value = (LightGrey, Just Yellow, MoveRight)}
-  , { key = (LightGrey, Just Green), 
-      value = (LightGrey, Just Green, MoveRight)}
-  , { key = (LightGrey, Just Blue), 
-      value = (LightGrey, Just Blue, MoveRight)}
-  , { key = (LightGrey, Nothing), 
+  [ { key = (White, Just Red), 
+      value = (White, Just Red, MoveRight)}
+  , { key = (White, Just Yellow), 
+      value = (White, Just Yellow, MoveRight)}
+  , { key = (White, Just Green), 
+      value = (White, Just Green, MoveRight)}
+  , { key = (White, Just Blue), 
+      value = (White, Just Blue, MoveRight)}
+  , { key = (White, Nothing), 
       value = (Grey, Just Red, MoveLeft)}
   , { key = (Grey, Just Red), 
       value = (Grey, Just Red, MoveLeft)}
-  , { key = (Grey, Just Orange), 
-      value = (Grey, Just Orange, MoveLeft)}
   , { key = (Grey, Just Yellow), 
       value = (Grey, Just Yellow, MoveLeft)}
   , { key = (Grey, Just Green), 
@@ -41,19 +55,329 @@ transTable =
   , { key = (Grey, Just Blue), 
       value = (Grey, Just Blue, MoveLeft)}
   , { key = (Grey, Nothing), 
-      value = (Brown, Just Blue, MoveRight)}
+      value = (Orange, Just Blue, MoveRight)}
   ] 
 
 
 input : List (Maybe BallOfWool)
 input = 
-  [Just Red, Nothing, Just Orange, Just Yellow, Just Green, Just Blue]
+  [Just Red, Nothing, Just Yellow, Just Green, Just Blue]
+
+------------------------------------------------------------------------------
+
+-- MODEL                                                                        
+type alias Model =                                                              
+  { windSize     : Window.Size                                                          
+  , clickPos     : Position
+  , machine      : Machine BallOfWool Kitten
+  , machineCfgs  : List (MachineCfg BallOfWool Kitten)
+  , transTable   : TransTable BallOfWool Kitten
+  }                                                                             
+                                                                                
+                                                                                
+type alias Position =                                                           
+  { x : Int, y : Int }                                                          
+                                                                                
+                                                                                
+-- MESSAGES                                                                     
+type Msg                                                                        
+  = Click Position                     
+  | WindowSize Window.Size                                                      
+                                                                                
+                                                                                
+init : Machine BallOfWool Kitten -> TransTable BallOfWool Kitten -> 
+       List (Maybe BallOfWool) -> (Model, Cmd Msg)                                                         
+init machine table inp =                                                                          
+  ( Model 
+      (Window.Size 1855 980) 
+      (Position 0 0) 
+      machine
+      [(initMachineCfg machine inp machine.initHeadPos)]
+      table
+  , Task.perform 
+        (\_ -> Debug.crash "task") WindowSize Window.size              
+  )                                                                             
+                                                                                
+                                                                                
+topMargin : Model -> String                                                     
+topMargin model =                                                               
+  ((toString ((model.windSize.height - mainRectH)//2))++"px")                       
+                                                                                
+                                                                                
+leftMargin : Model -> String                                                    
+leftMargin model =                                                              
+  ((toString ((model.windSize.width - mainRectW)//2))++"px")  
 
 
--- initial position of TM head (0 means 'above the first element')
-initHeadPos : Int
-initHeadPos = 3
+mainRectW : Int                                                                 
+mainRectW = 800                                                                 
 
 
-main =
-  text (debugRun testMachine input initHeadPos)
+mainRectH : Int                                                                 
+mainRectH = 600   
+
+
+-- VIEW                                                                         
+view : Model -> Html Msg                                                        
+view model =                                                                    
+  div                                                                           
+    [                                                                           
+      Html.Attributes.style                                                     
+            [ ( "position", "absolute" )                                        
+            , ( "top", (topMargin model) )                                      
+            , ( "left", (leftMargin model) )                                    
+            , ( "width", (toString mainRectW) )                                 
+            , ( "height", (toString mainRectH) )                                
+            , ( "border", "1px solid #000000" )                                 
+            , ( "background-color", "grey" )                                 
+            ]                                                                   
+    ]                                                                           
+    [ (addMainPanel model) ]  
+
+
+basketLeftMarginI : Int -> Int                                               
+basketLeftMarginI ind = ind * 100 + (ind+1)*13                                                                                                                  
+
+basketTopMarginI : Int                                                       
+basketTopMarginI = 410 
+
+
+basketLeftMarginS : Int -> String
+basketLeftMarginS ind =
+  toString (basketLeftMarginI ind) ++ "px"
+
+
+basketTopMarginS : String
+basketTopMarginS =
+  (toString basketTopMarginI) ++ "px"
+
+
+newBasket : Int -> Svg msg
+newBasket ind =
+  Svg.image                                                            
+    [ x (basketLeftMarginS ind)                                                       
+    , y basketTopMarginS                                            
+    , Svg.Attributes.width "100px"                                   
+    , Svg.Attributes.height "100px"                                  
+    , xlinkHref ("../img/basket.png")                                
+    ]                                                                
+    []    
+
+
+basketCreate : Int -> List (Svg msg) -> List (Svg msg)
+basketCreate n res =
+  if n > 0 then ( basketCreate (n-1) (res ++ [newBasket (n-1)]) )
+  else res
+
+
+tableW : String                                                                 
+tableW = (toString mainRectW)                                                   
+
+
+tableH : String                                                                 
+tableH = (toString mainRectW) 
+
+
+tableCreate : List (Svg msg)
+tableCreate =
+  [ Svg.image                                                            
+      [ x "0"                                                          
+      , y "0"                                                          
+      , Svg.Attributes.width tableW                                    
+      , Svg.Attributes.height tableH                                   
+      , xlinkHref ("../img/table.jpg")                                 
+      ]                                                                
+      [] 
+  ]
+
+
+mirrorCreate : List (Svg msg) 
+mirrorCreate =
+  [ Svg.image                                                            
+      [ x "10"                                                         
+      , y "10"                                                         
+      , Svg.Attributes.width "260px"                                   
+      , Svg.Attributes.height "390px"                                  
+      , xlinkHref ("../img/mirror1.png")                               
+      ]                                                                
+      []                                                               
+  ]  
+
+
+ballLeftMarginI : Int -> Int
+ballLeftMarginI ind =
+  (basketLeftMarginI ind) + 27
+
+
+ballTopMarginI : Int
+ballTopMarginI =
+  (basketTopMarginI) + 15
+
+
+ballLeftMarginS : Int -> String                                                           
+ballLeftMarginS ind =                                                               
+  (toString (ballLeftMarginI ind) ++ "px")
+
+
+ballTopMarginS : String                                                            
+ballTopMarginS =                                                                
+   (toString (ballTopMarginI) ++ "px")
+
+
+newBall : Int -> Maybe (Maybe BallOfWool) -> Svg msg
+newBall ind inpVal =
+  let 
+    color = (getBallColor inpVal)
+  in
+    Svg.image                                                            
+        [ x (ballLeftMarginS ind)
+        , y (ballTopMarginS)                                                        
+        , Svg.Attributes.width "50px"                                    
+        , Svg.Attributes.height "50px"                                   
+        , xlinkHref ("../img/" ++ color ++ "Ball.png")                                
+        ]                                                                
+        []
+
+
+getBallColor : Maybe (Maybe BallOfWool) -> String
+getBallColor inpVal =
+  case inpVal of
+    Just (Just Red) -> "Red"
+    Just (Just Yellow) -> "Yellow"
+    Just (Just Green) -> "Green"
+    Just (Just Blue) -> "Blue"
+    _ -> "Black"
+
+
+ballCreate : Int -> List (Svg msg) -> List (Maybe BallOfWool) -> Int 
+             -> List (Svg msg)                          
+ballCreate n res tape hpos =              
+  let 
+    inpVal = head (drop (n-1) tape)
+  in
+    if n > 0 
+       then if inpVal == Just (Nothing) 
+               then (ballCreate (n-1) res tape hpos)
+               else 
+                 let
+                   updRes = (res ++ [newBall (n-1+hpos) inpVal]) 
+                 in 
+                   (ballCreate (n-1) updRes tape hpos)               
+    else res  
+
+
+catLeftMarginI : Int -> Int                                                    
+catLeftMarginI hpos =                                                           
+    (basketLeftMarginI hpos) - 80                                                  
+
+
+catTopMarginI : Int                                                            
+catTopMarginI = 
+  basketTopMarginI + 10
+
+
+catLeftMarginS : Int -> String                                                 
+catLeftMarginS ind =                                                           
+    (toString (catLeftMarginI ind) ++ "px")                                      
+
+
+catTopMarginS : String                                                         
+catTopMarginS =                                                                
+     (toString (catTopMarginI) ++ "px") 
+
+
+catCreate : Int -> List (Svg msg)
+catCreate hpos =
+  [ Svg.image                                                             
+        [ x (catLeftMarginS hpos)                                                         
+        , y catTopMarginS                                                        
+        , Svg.Attributes.width "170px"                                   
+        , Svg.Attributes.height "170px"                                  
+        --, xlinkHref ("../img/SaimonGrey.png")
+        , xlinkHref ("../img/GreySaimonThink.png")
+        ]                                                                
+        []
+  ]
+
+
+emptyTape : TapeCfg BallOfWool
+emptyTape =
+  { leftSyms = empty
+  , currSym = Nothing
+  , rightSyms = empty
+  }                                                                             
+                                                                                          
+                                                                                
+emptyMCfg : MachineCfg BallOfWool Kitten 
+emptyMCfg =                                                     
+  { currState = White
+  , tapeCfg = emptyTape                                                       
+  } 
+
+
+addMainPanel : Model -> Html Msg                                                
+addMainPanel model =     
+  let
+    {-
+    tape = case (head (model.machineCfgs)) of
+              Just ini -> ini
+              Nothing -> emptyMCfg 
+    -}
+    tape = [Just Red, Nothing, Just Yellow, Just Green, Just Blue]
+    hpos = model.machine.initHeadPos
+  in
+    Svg.svg                                                                       
+        [ version "1.1"                                                           
+        , Svg.Attributes.width  (toString mainRectW)                              
+        , Svg.Attributes.height (toString mainRectH)                              
+        , x "0"                                                                   
+        , y "0"                                                                   
+        , viewBox ("0 0 " ++ (toString mainRectW) ++ " " ++ (toString mainRectH)) 
+        ]                                                                         
+        (  
+          tableCreate
+          ++
+          mirrorCreate
+          ++
+          (basketCreate 7 [])
+          ++
+          (ballCreate 7 [] tape hpos)
+          ++
+          (catCreate hpos)
+        )    
+
+
+-- UPDATE                                                                       
+update : Msg -> Model -> ( Model, Cmd Msg )                                     
+update msg model =                                                              
+  case msg of                                                                   
+    Click pos ->  
+      let 
+        p = ( Position                                                                  
+              ( pos.x - ((model.windSize.width - mainRectW)//2) )                       
+              ( pos.y - ((model.windSize.height - mainRectH)//2) )                      
+            )                                                                           
+      in 
+        ( { model | clickPos = p }, Cmd.none)                                                
+    WindowSize { width, height } ->                                               
+        ( { model | windSize = (Window.Size (width) (height)) }, Cmd.none )   
+
+
+-- SUBSCRIPTIONS                                                                
+subscriptions : Model -> Sub Msg                                                
+subscriptions model =                                                           
+  Sub.batch                                                                     
+    [ Mouse.clicks Click                                                        
+    , Window.resizes WindowSize                                                 
+    ] 
+
+
+-- MAIN                                                                         
+main : Program Never
+main =                                                                          
+  Html.App.program                                                              
+    { init = (init machine transTable input) 
+    , view = view                                                               
+    , update = update                                                           
+    , subscriptions = subscriptions                                             
+    }  
